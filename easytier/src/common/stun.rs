615bd1a84a1c70ccaecdad29a5e7b31842505214
yeyun <41390299+yeyun1999@users.noreply.github.com>
2025-05-21 21:50:58 +0800

    Update stun.rs
     "stun.voipstunt.com",
     "stun.nextcloud.com"

--- easytier/src/common/stun.rs
+++ easytier/src/common/stun.rs
@@ -787,7 +787,8 @@ impl StunInfoCollector {
         // NOTICE: we may need to choose stun stun server based on geo location
         // stun server cross nation may return a external ip address with high latency and loss rate
         vec![
-            "stun.nextcloud.com",
+            "stun.voipstunt.com",
+            "stun.nextcloud.com"
         ]
         .iter()
         .map(|x| x.to_string())
